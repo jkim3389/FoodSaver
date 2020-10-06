bd3b91e14548b6883add68f8589bc262f7c7ac42
juntae <jkim3389@gatech.edu>
2020-10-06 16:21:02 -0400

    feat(Azure): be able to fetch data and persist data as backend service

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from "./components/HomeScreen";
+import FridgeView from "./components/FridgeView";
+import TakePic from "./components/TakePic";
 
+
+const Stack = createStackNavigator();
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen name="Home2" component={HomeScreen}/>
+              <Stack.Screen name="FridgeView" component={FridgeView}/>
+              <Stack.Screen name="TakePic" component={TakePic}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/HomeScreen.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
+import AsyncStorage from '@react-native-community/async-storage';
+import { clockRunning } from "react-native-reanimated";
+
+export default function HomeScreen(props) {
+
+    const clearAll = async () => {
+        try {
+          await AsyncStorage.clear()
+          const data = await AsyncStorage.getItem('items')
+          console.log("after clear : ", data)
+        } catch(e) {
+          // clear error
+        }
+      
+        console.log('Done.')
+      }
+      
+    return (
+        <View style={styles.container}>
+            <View
+                style={{
+                    height: "100%",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            >
+                <TouchableOpacity style={styles.button}>
+                    <Button title="View My Fridge" onPress={()=>props.navigation.navigate("FridgeView")} />
+                    <Button title="Take a pic" onPress={()=>props.navigation.navigate("TakePic")}/>
+
+                    <Button title="Clear DAta" onPress={()=>{clearAll()}}/>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        // backgroundColor: "green"
+    },
+    button: {
+        height: "20%",
+
+        borderColor: "white",
+        borderWidth: 2,
+        justifyContent: "space-evenly",
+    },
+    text: {
+        marginTop: 30,
+        backgroundColor: "red",
+        alignItems: "center",
+        color: "white",
+    },
+});
