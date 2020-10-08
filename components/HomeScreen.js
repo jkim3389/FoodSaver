270ee31b81fdfa62ee01232bc63d38d82b8d3eaf
Heejoo Cho <joheeju@gatech.edu>
2020-10-07 21:36:12 -0400

    try to add item's image (in progress)

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { ImageBackground, StyleSheet, Text, View, Button, TouchableOpacity, Alert } from "react-native";
+import {StatusBar,Image, ImageBackground, StyleSheet, View, Button, TouchableOpacity, Alert } from "react-native";
 import AsyncStorage from '@react-native-community/async-storage';
 import image from "../assets/background.jpg"
+import logo from "../assets/logo.png"
+import { greaterThan } from "react-native-reanimated";
 
 export default function HomeScreen(props) {
 
@@ -18,13 +20,15 @@ export default function HomeScreen(props) {
       
     return (
         <View style={styles.container}>
-                <ImageBackground source={image} style={styles.image}>
+            <ImageBackground source={image} style={styles.image}>
+                <StatusBar translucent barStyle="light-content" backgroundColor="transparent"/>
+                <Image source={logo}  style={styles.logo}/>
                 <TouchableOpacity style={styles.button}>
-                    <Button title="View My Fridge" onPress={()=>props.navigation.navigate("ViewItems")} />
-                    <Button title="Add Items" onPress={()=>props.navigation.navigate("AddItems")}/>
+                    <Button title="View My Fridge" onPress={()=>props.navigation.navigate("My Fridge")} />
+                    <Button title="Add Items" onPress={()=>props.navigation.navigate("Add Items")}/>
                     <Button title="Clear Data" onPress={()=>{clearAll()}}/>
                 </TouchableOpacity>
-                </ImageBackground>
+            </ImageBackground>
         </View>
     );
 }
@@ -34,21 +38,22 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     button: {
+        marginTop: -20,
         height: "20%",
-
-        borderColor: "white",
-        borderWidth: 2,
+        // borderColor: "white",
+        // borderWidth: 2,
         justifyContent: "space-evenly",
     },
     image: {
         flex: 1,
         resizeMode: "cover",
-        justifyContent: "center"
+        justifyContent: "center",
       },
-    text: {
-        marginTop: 30,
-        backgroundColor: "red",
+    logo: { 
+        top: -10,
+        width: 400, 
+        height: 400, 
+        justifyContent: "center", 
         alignItems: "center",
-        color: "white",
-    },
+    }
 });
