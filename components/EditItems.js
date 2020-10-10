298fb73eb3f85c1e931cf28074b7442aea838c09
Woongrae Cho <woonglae@gmail.com>
2020-10-09 22:20:34 -0700

    Add EditItem page to edit items

---
+++ components/EditItems.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Image, ImageBackground, View, Text, FlatList, StyleSheet } from "react-native";
+
+
+export default function EditItems() {
+    return (
+        <Text>Help ME!!!!</Text>
+    )
+}
