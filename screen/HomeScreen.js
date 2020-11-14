896f52fed888f7d76fb324c9f869ba1016c2a46d
Juntae Kim <jkim3389@gatech.edu>
2020-11-14 17:17:49 -0500

    chore: saving the files

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -1,197 +1,181 @@
 import React, { useEffect, useState, useRef } from "react";
 import {
-    StatusBar,
-    Image,
-    StyleSheet,
-    View,
-    TouchableOpacity,
-    Text,
-    Alert,
+  StatusBar,
+  Image,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Text,
+  Alert,
 } from "react-native";
 import logo from "../assets/logo.png";
 import setting from "../assets/setting.png";
 import Background from "../components/Background";
 import { clearData, clearItems } from "../utils/storageManager";
-<<<<<<< HEAD
-import { auth } from "../utils/config";
-=======
-import { db } from "../utils/config";
-import Constants from 'expo-constants';
-import * as Permissions from 'expo-permissions';
-import * as Notifications from 'expo-notifications';
+import { db, auth } from "../utils/config";
+import Constants from "expo-constants";
+import * as Permissions from "expo-permissions";
+import * as Notifications from "expo-notifications";
 
 Notifications.setNotificationHandler({
-    handleNotification: async () => ({
-        shouldShowAlert: true,
-        shouldPlaySound: false,
-        shouldSetBadge: false,
-    }),
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
 });
 
->>>>>>> 4f511b6eaebe26bdbb55457497ec7a42cebb0bba
 export default function HomeScreen(props) {
-    const onSignout = () => {
-        auth.signOut()
-            .then((res) => {
-                Alert.alert("Signed out!");
-                props.navigation.navigate("LogIn");
-            })
-            .catch((err) => Alert.alert(err.message));
-    };
+  const onSignout = () => {
+    auth
+      .signOut()
+      .then((res) => {
+        Alert.alert("Signed out!");
+        props.navigation.navigate("LogIn");
+      })
+      .catch((err) => Alert.alert(err.message));
+  };
 
-    const onClearHandler = () => {
-        clearData();
-        clearItems();
-    };
+  const onClearHandler = () => {
+    clearData();
+    clearItems();
+  };
 
-    const [expoPushToken, setExpoPushToken] = useState('');
-    const [notification, setNotification] = useState(false);
-    const notificationListener = useRef();
-    const responseListener = useRef();
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
 
-    useEffect(() => {
-        registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
-        // This listener is fired whenever a notification is received while the app is foregrounded
-        notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-            setNotification(notification);
-        });
-        // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
-        responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-            console.log(response);
-        });
-            return () => {
-                Notifications.removeNotificationSubscription(notificationListener);
-                Notifications.removeNotificationSubscription(responseListener);
-            };
-    }, []);
-
-    return (
-        <View style={styles.container}>
-            <Background>
-                <StatusBar
-                    translucent
-                    barStyle="light-content"
-                    backgroundColor="transparent"
-                />
-                <TouchableOpacity
-                    onPress={() =>props.navigation.navigate("Setting")}
-                >
-                    <Image source={setting} style={styles.setting} />
-                </TouchableOpacity>
-                <Image source={logo} style={styles.logo} />
-                <TouchableOpacity
-                    style={styles.button}
-                    onPress={() => props.navigation.navigate("MyFridge")}
-                >
-                    <Text style={styles.buttonText}>View My Fridge</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={styles.button}
-                    onPress={() => props.navigation.navigate("Add Items")}
-                >
-                    <Text style={styles.buttonText}>Add Items</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.button} onPress={onSignout}>
-                    <Text style={styles.buttonText}>Sign Out</Text>
-                </TouchableOpacity>
-            </Background>
-        </View>
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token)
     );
-}
-<<<<<<< HEAD
-export const screenOptions = (navData) => {
-    return {
-        title: "Home",
-        headerShown: false,
+    // This listener is fired whenever a notification is received while the app is foregrounded
+    notificationListener.current = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        setNotification(notification);
+      }
+    );
+    // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log(response);
+      }
+    );
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener);
+      Notifications.removeNotificationSubscription(responseListener);
     };
-};
-=======
+  }, []);
 
+  return (
+    <View style={styles.container}>
+      <Background>
+        <StatusBar
+          translucent
+          barStyle="light-content"
+          backgroundColor="transparent"
+        />
+        <TouchableOpacity onPress={() => props.navigation.navigate("Setting")}>
+          <Image source={setting} style={styles.setting} />
+        </TouchableOpacity>
+        <Image source={logo} style={styles.logo} />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => props.navigation.navigate("MyFridge")}
+        >
+          <Text style={styles.buttonText}>View My Fridge</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => props.navigation.navigate("Add Items")}
+        >
+          <Text style={styles.buttonText}>Add Items</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={onSignout}>
+          <Text style={styles.buttonText}>Sign Out</Text>
+        </TouchableOpacity>
+      </Background>
+    </View>
+  );
+}
+export const screenOptions = (navData) => {
+  return {
+    title: "Home",
+    headerShown: false,
+  };
+};
 
 async function registerForPushNotificationsAsync() {
-    let token;
-    if (Constants.isDevice) {
-        const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
-        let finalStatus = existingStatus;
-        if (existingStatus !== 'granted') {
-            const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-            finalStatus = status;
-        }
-        if (finalStatus !== 'granted') {
-            alert('Failed to get push token for push notification!');
-            return;
-        }
-        token = (await Notifications.getExpoPushTokenAsync()).data;
-        // console.log(token);
-        let uid = Constants.installationId;
-        db.ref("users_expo_token").child(uid).update({
-            expoPushToken: token
-        });
-    } else {
-        alert('Must use physical device for Push Notifications');
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Permissions.getAsync(
+      Permissions.NOTIFICATIONS
+    );
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
     }
-    if (Platform.OS === 'android') {
-        Notifications.setNotificationChannelAsync('default', {
-            name: 'default',
-            importance: Notifications.AndroidImportance.MAX,
-            vibrationPattern: [0, 250, 250, 250],
-            lightColor: '#FF231F7C',
-        });
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
     }
-    return token;
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    // console.log(token);
+    let uid = Constants.installationId;
+    db.ref("users_expo_token").child(uid).update({
+      expoPushToken: token,
+    });
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+  return token;
 }
 
->>>>>>> 4f511b6eaebe26bdbb55457497ec7a42cebb0bba
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    button: {
-        top: -30,
-        height: "5%",
-        width: "45%",
-        justifyContent: "space-evenly",
-<<<<<<< HEAD
-        alignSelf: "center",
-        backgroundColor: "rgba(190, 223, 83, .5)",
-=======
-        alignSelf: 'center',
-        backgroundColor: 'rgba(190, 223, 83, .5)',
->>>>>>> 4f511b6eaebe26bdbb55457497ec7a42cebb0bba
-        marginTop: 20,
-        borderRadius: 30,
-    },
-    buttonText: {
-        fontWeight: "bold",
-        fontSize: 18,
-<<<<<<< HEAD
-        color: "#1D1C1A",
-        textAlign: "center",
-        textTransform: "uppercase",
-=======
-        color: '#1D1C1A',
-        textAlign: 'center',
-        textTransform: 'uppercase'
->>>>>>> 4f511b6eaebe26bdbb55457497ec7a42cebb0bba
-    },
-    logo: {
-        top: -30,
-        width: 370,
-        height: 370,
-<<<<<<< HEAD
-        alignSelf: "center",
-=======
-        alignSelf: 'center',
->>>>>>> 4f511b6eaebe26bdbb55457497ec7a42cebb0bba
-        justifyContent: "center",
-        marginBottom: -30,
-    },
-    setting: {
-        top: -95,
-        right: 20,
-        alignSelf: "flex-end",
-        // position: "absolute",
-        height: 50,
-        width: 50,
-    }
+  container: {
+    flex: 1,
+  },
+  button: {
+    top: -30,
+    height: "5%",
+    width: "45%",
+    justifyContent: "space-evenly",
+    alignSelf: "center",
+    backgroundColor: "rgba(190, 223, 83, .5)",
+    marginTop: 20,
+    borderRadius: 30,
+  },
+  buttonText: {
+    fontWeight: "bold",
+    fontSize: 18,
+    color: "#1D1C1A",
+    textAlign: "center",
+    textTransform: "uppercase",
+  },
+  logo: {
+    top: -30,
+    width: 370,
+    height: 370,
+    alignSelf: "center",
+    justifyContent: "center",
+    marginBottom: -30,
+  },
+  setting: {
+    top: -95,
+    right: 20,
+    alignSelf: "flex-end",
+    // position: "absolute",
+    height: 50,
+    width: 50,
+  },
 });
--- utils/storageManager.js
+++ utils/storageManager.js
@@ -1,12 +1,11 @@
 import AsyncStorage from "@react-native-community/async-storage";
 import { Alert } from "react-native";
 import { db, auth } from "./config";
-import * as Notifications from 'expo-notifications';
-import Constants from 'expo-constants';
-
+import * as Notifications from "expo-notifications";
+import Constants from "expo-constants";
 
 state = {
-    items: [],
+  items: [],
 };
 
 // //Local Storage
@@ -59,7 +58,6 @@ state = {
 //     }
 // }
 
-
 /* 
  
     Version 1
@@ -111,94 +109,106 @@ export function getUID(){
 
 */
 
-
 //V2 ------------ THIS IS FIREBASE VERSION UNDER USER ID
 
 export async function removeDataByOne(key) {
-    try {
-        db.ref(`/${getUID()}/items`).remove();
-        db.ref(`/${getUID()}/users`).remove();
-        Alert.alert(`Cleared local data`);
-    } catch (e) {
-        console.log("error occured during store data", e);
-    }
+  try {
+    db.ref(`/${getUID()}/items`).remove();
+    db.ref(`/${getUID()}/users`).remove();
+    Alert.alert(`Cleared local data`);
+  } catch (e) {
+    console.log("error occured during store data", e);
+  }
 }
 
 export function addNewItem(key, value) {
-    db.ref(`/${getUID()}/items`).child(key).set(value);
+  db.ref(`/${getUID()}/items`).child(key).set(value);
 }
 
 export function fbReadAllData() {
-    db.ref(`/${getUID()}/items`).on("value", (dataSnapshot) => {
-        let data = dataSnapshot.val() ? dataSnapshot.val() : {};
-        let items = Object.values(data);
-        return items;
-    });
+  db.ref(`/${getUID()}/items`).on("value", (dataSnapshot) => {
+    let data = dataSnapshot.val() ? dataSnapshot.val() : {};
+    let items = Object.values(data);
+    return items;
+  });
 }
 
 export function clearItems() {
-    try {
-        db.ref(`/${getUID()}/items`).remove();
-        Alert.alert(`Cleared local data`);
-    } catch (e) {
-        console.log(e);
-        Alert.alert(`Error`);
-    }
+  try {
+    db.ref(`/${getUID()}/items`).remove();
+    Alert.alert(`Cleared local data`);
+  } catch (e) {
+    console.log(e);
+    Alert.alert(`Error`);
+  }
 }
 
 export function fbRemoveDataByOne(key) {
-    try {
-        db.ref(`/${getUID()}/items` + key).remove();
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    db.ref(`/${getUID()}/items` + key).remove();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-export async function schedulePushNotification(notifDay, expireDay, productname, itemID) {
-    var cancel_expire_day_notif
-    var cancel_notif_day
-    db.ref('/users').on("value", (dataSnapshot) => {
-        let data = dataSnapshot.val() ? dataSnapshot.val() : {};
-        let users = Object.values(data);
-        try {
-            cancel_expire_day_notif = users[0][itemID]['expire_day_notif_id'];
-            cancel_notif_day = users[0][itemID]['days_before_notif_id'];
-        } catch(e) {
-            // console.log(e)
-        }
-    });
-    await Notifications.cancelScheduledNotificationAsync(cancel_expire_day_notif);
-    await Notifications.cancelScheduledNotificationAsync(cancel_notif_day);
-    const expire_day_notif = await Notifications.scheduleNotificationAsync({
-        content: {
-            title: "FoodSaver",
-            body: productname + ' is expiring soon!',
-            data: { data: 'goes here' },
-        },
-        trigger: { 
-            year: notifDay.getFullYear(),
-            month: notifDay.getMonth()+1,
-            day: notifDay.getDate(),
-            hour: 16,
-            minute: 41,
-            second: 30,
-        },
-    });
-    const days_before_notif = await Notifications.scheduleNotificationAsync({
-        content: {
-            title: "FoodSaver",
-            body: productname + ' is expiring today!',
-            data: { data: 'goes here' },
-        },
-        trigger: { 
-            year: expireDay.getFullYear(),
-            month: expireDay.getMonth()+1,
-            day: expireDay.getDate(),
-            hour: 16,
-            minute: 41,
-            second: 30,
-        },
-    });
-    db.ref('/users').child(Constants.installationId).child(itemID).child('expire_day_notif_id').set(expire_day_notif)
-    db.ref('/users').child(Constants.installationId).child(itemID).child('days_before_notif_id').set(days_before_notif)
+export async function schedulePushNotification(
+  notifDay,
+  expireDay,
+  productname,
+  itemID
+) {
+  var cancel_expire_day_notif;
+  var cancel_notif_day;
+  db.ref("/users").on("value", (dataSnapshot) => {
+    let data = dataSnapshot.val() ? dataSnapshot.val() : {};
+    let users = Object.values(data);
+    try {
+      cancel_expire_day_notif = users[0][itemID]["expire_day_notif_id"];
+      cancel_notif_day = users[0][itemID]["days_before_notif_id"];
+    } catch (e) {
+      // console.log(e)
+    }
+  });
+  await Notifications.cancelScheduledNotificationAsync(cancel_expire_day_notif);
+  await Notifications.cancelScheduledNotificationAsync(cancel_notif_day);
+  const expire_day_notif = await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "FoodSaver",
+      body: productname + " is expiring soon!",
+      data: { data: "goes here" },
+    },
+    trigger: {
+      year: notifDay.getFullYear(),
+      month: notifDay.getMonth() + 1,
+      day: notifDay.getDate(),
+      hour: 16,
+      minute: 41,
+      second: 30,
+    },
+  });
+  const days_before_notif = await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "FoodSaver",
+      body: productname + " is expiring today!",
+      data: { data: "goes here" },
+    },
+    trigger: {
+      year: expireDay.getFullYear(),
+      month: expireDay.getMonth() + 1,
+      day: expireDay.getDate(),
+      hour: 16,
+      minute: 41,
+      second: 30,
+    },
+  });
+  db.ref("/users")
+    .child(Constants.installationId)
+    .child(itemID)
+    .child("expire_day_notif_id")
+    .set(expire_day_notif);
+  db.ref("/users")
+    .child(Constants.installationId)
+    .child(itemID)
+    .child("days_before_notif_id")
+    .set(days_before_notif);
 }
